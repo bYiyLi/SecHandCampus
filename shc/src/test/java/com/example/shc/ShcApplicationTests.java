a32a701b31ddd482a3ce2733a92517b5da1120ce
Yi <193463771@qq.com>
2020-06-03 21:54:48 +0800

    init

---
+++ shc/src/test/java/com/example/shc/ShcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
